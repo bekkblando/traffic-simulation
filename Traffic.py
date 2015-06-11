5823e73ca44011cb3a7840ffd3d69fd003079f71
Bekk Blando <bekkblando@gmail.com>
2015-06-11 14:04:50 -0400

    Defining Classes

---
+++ Traffic.py
@@ -0,0 +1,27 @@
+class Road:
+
+    def __init__():
+        pass
+
+
+class Car:
+
+    def __init__(self, position=0):
+        self.position = position
+        self.size = 5
+        self.accel = 2
+        self.max_speed = 120
+
+    def accelerate():
+        pass
+
+    def stop():
+        pass
+
+    def slow():
+        pass
+
+    def check_road():
+        pass
+
+    def
